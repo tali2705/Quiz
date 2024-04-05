18efe0b372a0d70cea22aa6b800cb38ef9ec094e
tali2705 <natalija8140@gmail.com>
2024-04-05 10:40:12 +0200

    ispravka jsona i pravljenje question komponente ispocetka

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -22,23 +22,8 @@ const Questions = () => {
     return (
         <>
             <h1>Quiz</h1>
-            {questions.map((question) => {
-                return (
-                    <div key={question.id}>
-                        <h3>Pitanje {question.id}: {question.question}</h3>
-                        <input type="radio" name="answer" id="A" value={question.A} />
-                        <label for="A">{question.A}</label>
-                        <br />
-                        <input type="radio" name="answer" id="B" value={question.B} />
-                        <label for="B">{question.B}</label>
-                        <br />
-                        <input type="radio" name="answer" id="C" value={question.C} />
-                        <label for="C">{question.C}</label>
 
-                        <hr />
-                    </div>
-                );
-            })}
+
 
 
 
